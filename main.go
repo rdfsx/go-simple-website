8281986fcd571b197785a578c5c25be79ddcc325
rdfsx <m315498@gmail.com>
2021-03-13 12:58:04 +0300

    add some oop

--- main.go
+++ main.go
@@ -6,8 +6,25 @@ import (
 	"net/http"
 )
 
+type User struct {
+	name                 string
+	age                  uint16
+	money                int16
+	avgGrades, happiness float64
+}
+
+func (u *User) getAllInfo() string {
+	return fmt.Sprintf("User name is: %s. He is %d and he has money equal: %d", u.name, u.age, u.money)
+}
+
+func (u *User) setNewName(newName string) {
+	u.name = newName
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
-	n, err := fmt.Fprintf(w, "Hello Go")
+	bob := &User{"Bob", 25, -50, 4.2, 0.8}
+	bob.setNewName("Alex")
+	n, err := fmt.Fprintf(w, bob.getAllInfo())
 	fmt.Println(n, err)
 }
 
@@ -23,5 +40,6 @@ func handleRequest() {
 }
 
 func main() {
+
 	handleRequest()
 }
