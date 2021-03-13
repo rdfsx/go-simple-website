5d701db17c4bac31ae898e0196ba70d458245b1a
rdfsx <m315498@gmail.com>
2021-03-13 15:37:23 +0300

    added html web page

--- main.go
+++ main.go
@@ -2,30 +2,42 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 type User struct {
-	name                 string
-	age                  uint16
-	money                int16
-	avgGrades, happiness float64
+	Name                 string
+	Age                  uint16
+	Money                int16
+	AvgGrades, Happiness float64
+	Hobbies              []string
 }
 
 func (u *User) getAllInfo() string {
-	return fmt.Sprintf("User name is: %s. He is %d and he has money equal: %d", u.name, u.age, u.money)
+	return fmt.Sprintf("User Name is: %s. He is %d and he has Money equal: %d", u.Name, u.Age, u.Money)
 }
 
 func (u *User) setNewName(newName string) {
-	u.name = newName
+	u.Name = newName
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	bob := &User{"Bob", 25, -50, 4.2, 0.8}
-	bob.setNewName("Alex")
-	n, err := fmt.Fprintf(w, bob.getAllInfo())
-	fmt.Println(n, err)
+	bob := &User{"Bob", 25, -50, 4.2, 0.8,
+		[]string{"football", "skate", "dance"}}
+	tmpl, err := template.ParseFiles("templates/home_page.html")
+	if err != nil {
+		return
+	}
+	if tmpl == nil {
+		return
+	}
+	err = tmpl.Execute(w, bob)
+	if err != nil {
+		return
+	}
+	fmt.Println(err)
 }
 
 func contactsPage(w http.ResponseWriter, r *http.Request) {
